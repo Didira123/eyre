715fa3561c01f5f800ec27f4816e8a232764c25a
Diego Rafael <ogeiddiego064@gmail.com>
2023-09-13 20:21:14 -0300

    Acoes:ExtraAPI não possui "RequestMapping(...)" nem "@RestController";Status:corrigido;

--- src/main/java/br/com/eyre/eyre/api/ExtraAPI.java
+++ src/main/java/br/com/eyre/eyre/api/ExtraAPI.java
@@ -1,12 +1,16 @@
 package br.com.eyre.eyre.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.eyre.eyre.bases.CrudBaseAPI;
 import br.com.eyre.eyre.entity.Extra;
 import br.com.eyre.eyre.service.ExtraService;
 import br.com.eyre.eyre.vo.ExtraVO;
 
+@RestController
+@RequestMapping("/api/extra")
 public class ExtraAPI extends CrudBaseAPI<Long, Extra, ExtraVO, ExtraService> {
 
 	@Autowired
