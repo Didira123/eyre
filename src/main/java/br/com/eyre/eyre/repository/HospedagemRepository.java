bfff174dff1dcdebba760a05c1229021c2f4d13e
Diego Rafael <ogeiddiego064@gmail.com>
2023-09-20 19:39:31 -0300

    Acoes:backup.sql do banco talvez funcional;Status:feito?;

--- src/main/java/br/com/eyre/eyre/repository/HospedagemRepository.java
+++ src/main/java/br/com/eyre/eyre/repository/HospedagemRepository.java
@@ -10,16 +10,16 @@ import br.com.eyre.eyre.repository.custom.HospedagemRepositoryCustom;
 
 public interface HospedagemRepository extends JpaRepository<Hospedagem, Long>, HospedagemRepositoryCustom{
 
-	@Query(value = "SELECT h FROM Hospedagem h "
+	@Query(value = "SELECT distinct h FROM Hospedagem h "
 			+ " INNER JOIN FETCH h.endereco he "
+			+ " INNER JOIN FETCH h.listMidias lm "
+			+ " INNER JOIN FETCH lm.midia mid "
 			+ " INNER JOIN FETCH h.listTransportes lt "
 			+ " INNER JOIN FETCH lt.transporte t "
 			+ " INNER JOIN FETCH t.listTransporteEnderecos lte "
 			+ " INNER JOIN FETCH lte.endereco te "
 			+ " INNER JOIN FETCH t.listTransporteDias ltd "
 			+ " INNER JOIN FETCH ltd.listTransporteDiaHorarios ltdh "
-			+ " INNER JOIN FETCH h.listMidias lm "
-			+ " INNER JOIN FETCH lm.midia mid "
 			+ " LEFT JOIN FETCH h.listExtras le "
 			+ " LEFT JOIN FETCH le.extra ext "
 			+ " LEFT JOIN FETCH h.listProximidades lp "
